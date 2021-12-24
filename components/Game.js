73c519b76d0245e59f813527c2ddac7760929817
siarhei <homerj112@yandex.ru>
2021-12-24 22:55:47 +0300

    added async functions

--- components/Game.js
+++ components/Game.js
@@ -8,51 +8,45 @@ import { randomizer, createElement } from '../utils/utils.js';
 export class Game {
     constructor() {
         this.player1 = new Player({
+            ...JSON.parse(localStorage.getItem('player1')),
             player: 1,
-            name: 'Scorpio',
-            hp: 100,
-            img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
         });
         this.player2 = new Player({
+            ...JSON.parse(localStorage.getItem('player2')),
             player: 2,
-            name: 'Subzero',
-            hp: 100,
-            img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
         });
         this.$arena = document.querySelector('.arenas');
         this.$fightButton = document.querySelector('.button');
         this.$formFight = document.querySelector('.control');
     }
 
-    enemyAttack = () => {
-        const hit = ATTACK[randomizer(0, 2)];
-        const defense = ATTACK[randomizer(0, 2)];
-        return {
-            value: randomizer(1, HIT[hit]),
-            hit,
-            defense,
-        };
-    };
-
-    ownAttack = () => {
+    async attack() {
         const attack = {};
         for (let item of this.$formFight) {
-            if (item.checked && item.name === 'hit') {
-                attack.value = randomizer(1, HIT[item.value]);
-                attack.hit = item.value;
-            }
+            if (item.checked && item.name === 'hit') attack.hit = item.value;
 
-            if (item.checked && item.name === 'defence') {
+            if (item.checked && item.name === 'defence')
                 attack.defense = item.value;
-            }
+
             item.checked = false;
         }
-        return attack;
-    };
+        let response = await fetch(
+            'http://reactmarathon-api.herokuapp.com/api/mk/player/fight',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    hit: attack.hit,
+                    defence: attack.defense,
+                }),
+            }
+        ).then((data) => data.json());
+        return response;
+    }
 
-    fightAction = () => {
-        const enemy = this.enemyAttack();
-        const attack = this.ownAttack();
+    async fightAction() {
+        const attacks = await this.attack();
+        const enemy = attacks.player1;
+        const attack = attacks.player2;
 
         if (enemy.hit !== attack.defense) {
             this.player1.showDamage(enemy.value);
@@ -63,7 +57,7 @@ export class Game {
             this.player2.showDamage(attack.value);
             generateLogs('hit', this.player1, this.player2, attack.value);
         } else generateLogs('defence', this.player1, this.player2);
-    };
+    }
 
     showResult = () => {
         if (this.player1.hp === 0 || this.player2.hp === 0) {
@@ -92,10 +86,15 @@ export class Game {
         $reloadWrap.append($reloadButton);
         this.$arena.append($reloadWrap);
 
-        $reloadButton.addEventListener('click', () => window.location.reload());
+        $reloadButton.addEventListener(
+            'click',
+            () => (window.location = 'index.html')
+        );
     };
 
     start = () => {
+        this.$arena.classList.add(`arena${randomizer(1, 5)}`);
+
         this.$arena.append(
             createPlayer(this.player1),
             createPlayer(this.player2)
@@ -105,8 +104,7 @@ export class Game {
 
         this.$formFight.addEventListener('submit', (event) => {
             event.preventDefault();
-            this.fightAction();
-            this.showResult();
+            this.fightAction().then(() => this.showResult());
         });
     };
 }
---
+++ index.js
@@ -0,0 +1,85 @@
+const $parent = document.querySelector('.parent');
+const $player = document.querySelector('.player');
+const $player2 = document.querySelector('.player2');
+
+const createElement = (tag, className) => {
+    const $tag = document.createElement(tag);
+    if (className) {
+        if (Array.isArray(className)) {
+            className.forEach((item) => {
+                $tag.classList.add(item);
+            });
+        } else {
+            $tag.classList.add(className);
+        }
+    }
+
+    return $tag;
+};
+
+function createEmptyPlayerBlock() {
+    const el = createElement('div', ['character', 'div11', 'disabled']);
+    const img = createElement('img');
+    img.src = 'http://reactmarathon-api.herokuapp.com/assets/mk/avatar/11.png';
+    el.appendChild(img);
+    $parent.appendChild(el);
+}
+
+async function init() {
+    localStorage.removeItem('player1');
+    localStorage.removeItem('player2');
+
+    const players = await fetch(
+        'https://reactmarathon-api.herokuapp.com/api/mk/players'
+    ).then((res) => res.json());
+    const player2 = await fetch(
+        'https://reactmarathon-api.herokuapp.com/api/mk/player/choose'
+    ).then((res) => res.json());
+
+    let imgSrc = null;
+    createEmptyPlayerBlock();
+
+    players.forEach((item) => {
+        const el = createElement('div', ['character', `div${item.id}`]);
+        const img = createElement('img');
+
+        el.addEventListener('mousemove', () => {
+            if (imgSrc === null) {
+                imgSrc = item.img;
+                const $img = createElement('img');
+                $img.src = imgSrc;
+                $player.appendChild($img);
+            }
+        });
+
+        el.addEventListener('mouseout', () => {
+            if (imgSrc) {
+                imgSrc = null;
+                $player.innerHTML = '';
+            }
+        });
+
+        el.addEventListener('click', () => {
+            console.log(player2);
+            localStorage.setItem('player1', JSON.stringify(item));
+            localStorage.setItem('player2', JSON.stringify(player2));
+            el.classList.add('active');
+
+            const $img2 = createElement('img');
+            $img2.src = player2.img;
+            $player2.appendChild($img2);
+
+            setTimeout(() => {
+                window.location.pathname = 'arenas.html';
+            }, 1000);
+        });
+
+        img.src = item.avatar;
+        img.alt = item.name;
+
+        el.appendChild(img);
+        $parent.appendChild(el);
+    });
+}
+
+init();
